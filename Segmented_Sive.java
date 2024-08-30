25e2b96d025a12d81db8bf62f1a043b774e44947
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-08-31 05:20:59 +0530

    Update Segmented_Sive.java

--- Segmented_Sive.java
+++ Segmented_Sive.java
@@ -1,33 +1,55 @@
-import java.util.Scanner;
 import java.util.*;
-public class sive
-{
-    public static void main(String args[])
-    {
+
+public class seg {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Start : ");
-        int s = input.nextInt();
+        System.out.print("Enter start : ");
+        int start = input.nextInt();
 
-        System.out.print("Enter End : ");
+        System.out.print("Enter end  :");
         int n = input.nextInt();
 
-        boolean bool[] = new boolean[n];
-        
-        for(int i=0;i<n;i++)
-            bool[i]=true;
+        int size = (int)Math.sqrt(n);
 
-        //finding primes
-        for(int i=2;i<n;i++)
+        boolean prim[]=new boolean[size+1];
+
+        for(int i=0;i<size;i++)
+            prim[i]=true;
+        
+        for(int i=2;i<size;i++)
         {
-            if(bool[i]==true)
-                for(int j=(i*i);j<n;j+=i)
-                    bool[j]=false;
+            if(prim[i]==true)
+                for(int j=(i*i);j<size;j+=i)
+                    prim[j]=false;
         }
 
-        //printing
-        for(int i=s;i<n;i++)
-            if(bool[i]==true)
-                System.out.print(i+" ");
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0;i<size;i++)
+            if(prim[i]==true)
+                arr.add(i);
+        
+
+        boolean segsive[]=new boolean[n-start+1];
+        Arrays.fill(segsive,true);
+
+        for (int p=2;p<arr.size();p++) 
+        {
+            for(int j=start;j<=n;j++)
+            {
+                if(j%arr.get(p)==0)
+                    segsive[j-start]=false;
+            }
+            
+        }
+
+        for(int i=0;i<(n-start+1);i++)
+        {
+            if(segsive[i]==true)
+            {
+                System.out.println(i+start);
+            }
+        }
     }
 }
