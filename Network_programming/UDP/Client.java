fc0825107bde2a82140fc92db07294ae9d81c45e
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-11-19 18:36:04 +0530

    Update Client.java

--- Network_programming/UDP/Client.java
+++ Network_programming/UDP/Client.java
@@ -1,21 +1,20 @@
-import java.util.*;
-import java.io.*;
+
 import java.net.*;
 
-public class c2 {
 
+public class client1 {
     public static void main(String[] args) {
         
         try
         {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter ip or domain name : ");
-            String val = sc.nextLine();
-            InetAddress ip = InetAddress.getByName("localhost");
+            DatagramSocket datagramSocket = new DatagramSocket(9000);
+            byte buffer[] = new byte[1024];
 
-            DatagramSocket datagramSocket = new DatagramSocket();
-            DatagramPacket datagramPacket = new DatagramPacket(val.getBytes(),val.length(),ip,9000);
-            datagramSocket.send(datagramPacket);
+            DatagramPacket datagramPacket = new DatagramPacket(buffer,1024);
+            datagramSocket.receive(datagramPacket);
+
+            String out = new String(datagramPacket.getData());
+            System.out.println(out);
         }
         catch(Exception e)
         {
