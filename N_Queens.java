1abd758fdca0ef546fa5200c01a851cae47b6e13
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-12-11 07:05:37 +0530

    Update N_Queens.java

--- N_Queens.java
+++ N_Queens.java
@@ -1,29 +1,49 @@
-public class t1 {
-    public static void main(String[] args) {
-        char[][] board = new char[8][8];
-        for (char[] row : board) java.util.Arrays.fill(row, '-');
-        solve(board, 0);
+import java.util.*;
+public class queen
+{
+    public static boolean issafe(char board[][],int row, int col)
+    {
+        for(int i=0;i<row;i++)
+        {
+            if(board[i][col]=='Q' || (col-row+i>=0 && board[i][col-row+i]=='Q') || (col+row-i<board.length && board[i][col+row-i]=='Q'))
+            {
+                return false;
+            }
+        }
+        return true;
+
     }
 
-    static boolean solve(char[][] board, int row) {
-        if (row == board.length) {
-            for (char[] r : board) System.out.println(java.util.Arrays.toString(r));
+    public static boolean solve(char board[][], int row)
+    {
+        if(row==board.length)
+        {
+            for(char r[] : board)
+            {
+                System.out.println(Arrays.toString(r));
+            }
             return true;
         }
-        for (int col = 0; col < board.length; col++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                if (solve(board, row + 1)) return true;
-                board[row][col] = '-';
+
+        for(int col=0;col<board.length;col++)
+        {
+            if(issafe(board,row,col))
+            {
+                board[row][col]='Q';
+                if(solve(board,row+1))
+                    return true;
+                board[row][col]='-';
             }
         }
         return false;
     }
+    
+    public static void main(String args[])
+    {
+        char board[][] = new char[8][8];
 
-    static boolean isSafe(char[][] board, int row, int col) {
-        for (int i = 0; i < row; i++)
-            if (board[i][col] == 'Q' || (col - row + i >= 0 && board[i][col - row + i] == 'Q') 
-            || (col + row - i < board.length && board[i][col + row - i] == 'Q')) return false;
-        return true;
+        for(char r[] : board)
+            Arrays.fill(r,'-');
+        solve(board,0);
     }
 }
