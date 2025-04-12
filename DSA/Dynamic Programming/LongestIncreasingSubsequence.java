4ed8459d40596bf2c602329b74f24a4749b05f55
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-04-12 09:27:06 +0530

    Update LongestIncreasingSubsequence.java

--- DSA/Dynamic Programming/LongestIncreasingSubsequence.java
+++ DSA/Dynamic Programming/LongestIncreasingSubsequence.java
@@ -1,47 +1,45 @@
 import java.util.*;
-import java.io.*;
 
 public class LongestIncreasingSubsequence
 {
-    public static void main(String args[])
-    {
-        int arr[] = {5,4,11,1,16,8};
-        int n = arr.length;
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(5,4,11,1,16,8));
+        int n = arr.size();
 
-        int dp[]= new int[n];
-        int hash[] = new int [n];
+        int dp[] = new int[n];
+        int prev[] = new int[n];
+
         Arrays.fill(dp,1);
-        Arrays.fill(hash,-1);
+        Arrays.fill(prev, -1);
 
         for(int i=0;i<n;i++)
         {
-            for(int prev=0;prev<i;prev++)
+            for(int j=0;j<i;j++)
             {
-                if(arr[prev]<arr[i] && dp[i]<1+dp[prev])
+                if(arr.get(j)<arr.get(i) && dp[i]<1+dp[j])
                 {
-                    dp[i]=1+dp[prev];
-                    hash[i]=prev;
+                    dp[i]=1+dp[j];
+                    prev[i]=j;
                 }
             }
         }
 
-        int max=1;
-        int idx=0;
-
+        int max=1,index=0;
         for(int i=0;i<n;i++)
         {
             if(dp[i]>max)
             {
                 max=dp[i];
-                idx=i;
+                index=i;
             }
         }
 
         ArrayList<Integer> seq = new ArrayList<>();
-        while(idx!=-1)
+        while(index!=-1)
         {
-            seq.add(arr[idx]);
-            idx=hash[idx];
+            seq.add(arr.get(index));
+            index=prev[index];
         }
 
         System.out.println("Length : "+max);
