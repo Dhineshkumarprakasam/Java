541b3d011028ac1bb7282dc8cd0af25425afcf22
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-04-12 09:10:04 +0530

    Update LongestIncreasingSubsequence.java

--- DSA/Dynamic Programming/LongestIncreasingSubsequence.java
+++ DSA/Dynamic Programming/LongestIncreasingSubsequence.java
@@ -5,43 +5,46 @@ public class LongestIncreasingSubsequence
 {
     public static void main(String args[])
     {
-        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,5,4));
-        ArrayList<Integer> seq = new ArrayList<>();
+        int arr[] = {5,4,11,1,16,8};
+        int n = arr.length;
 
-        int n = arr.size();
-        int ans[] = new int[n];
-        int prev[] = new int[n];
-
-        Arrays.fill(ans,1);
-        Arrays.fill(prev, -1);
-
-        for(int i=n-1;i>=0;i--)
-            for(int j=(i-1);j>=0;j--)
-                if(arr.get(i)>arr.get(j))
-                    if(ans[j]<ans[i]+1)
-                    {
-                        ans[j] = ans[i]+1;
-                        prev[j]=i;
-                    }
-
-        int max=ans[0];
-        int index=0;
+        int dp[]= new int[n];
+        int hash[] = new int [n];
+        Arrays.fill(dp,1);
+        Arrays.fill(hash,-1);
 
         for(int i=0;i<n;i++)
-            if(ans[i]>max)
-            {
-                max=ans[i];
-                index=i;
-            }
-
-        //sequence finding
-        while(index!=-1)
         {
-            seq.add(arr.get(index));
-            index=prev[index];
+            for(int prev=0;prev<i;prev++)
+            {
+                if(arr[prev]<arr[i] && dp[i]<1+dp[prev])
+                {
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
+                }
+            }
+        }
+
+        int max=1;
+        int idx=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]>max)
+            {
+                max=dp[i];
+                idx=i;
+            }
+        }
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        while(idx!=-1)
+        {
+            seq.add(arr[idx]);
+            idx=hash[idx];
         }
 
         System.out.println("Length : "+max);
-        System.out.println("Sequence "+seq);
+        System.out.println("Sequence : "+seq.reversed());
     }
 }
