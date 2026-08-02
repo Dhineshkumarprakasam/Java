00f36bb9843552fd5e3aedcb3c7f9ef77c5d383f
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-08-02 20:40:07 +0530

    Create gcd.java

---
+++ ACC/gcd.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int min = Math.min(a, b);
+        int gcd = 1;
+
+        for(int i = 1; i <= min; i++)
+        {
+            if(a % i == 0 && b % i == 0)
+            {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD = " + gcd);
+    }
+}
